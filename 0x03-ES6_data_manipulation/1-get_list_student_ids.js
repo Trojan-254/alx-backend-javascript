271d5157590f3317fb75409d2732143b518b715f
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-08 20:40:55 +0300

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+export default function getListStudentIds(students) {
+   if (!Array.isArray(students)) {
+       return [];
+   }
+
+   return students.map(students => students.id);
+}
