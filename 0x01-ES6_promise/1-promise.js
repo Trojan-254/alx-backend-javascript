abba2107d7ffe977fb6026328c6b02ff054a0892
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-13 21:49:50 +0300

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/**
+* return a promise
+*/
+
+
+export default function getFullResponseFromAPI(success) {
+   return new Promise((resolve, reject) => {
+     if (success) {
+       resolve ({
+         status: 200,
+         body: 'Success'
+       });
+     } else {
+       reject (new Error("The fake API is not working currently"));
+     }
+   });
+}
