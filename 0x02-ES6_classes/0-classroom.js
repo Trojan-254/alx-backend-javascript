7f2af506a7dfde8b211086d5b3c09610daf21b83
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-21 11:37:45 +0300

    0. You used to attend a place like this at some point, 1. Let's make some classrooms

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+* module for ClassRoom
+*/
+
+export default class ClassRoom {
+   constructor(maxStudentsSize) {
+     this._maxStudentsSize = maxStudentsSize;
+   }
+}
---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+* initializeRooms module
+*/
+
+
+import ClassRoom from "./0-classroom.js"
+
+
+export default function initializeRooms() {
+   const sizes = [19, 20, 34];
+   return sizes.map(size => new ClassRoom(size));
+}
