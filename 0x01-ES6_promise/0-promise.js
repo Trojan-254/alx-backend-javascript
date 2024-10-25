df8840bd0f56dbfbbe93bff15be109d73776c5e4
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-26 01:55:19 +0300

    0. Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,4 @@
-#!/usr/bin/node
-/**
-*Returns a promise
-*/
-
+/* eslint-disable */
 export default function getResponseFromAPI() {
-   return new Promise((resolve, reject) => {
-     resolve('Api response')
-   });
+	return new Promise((resolve, reject) => {});
 }
