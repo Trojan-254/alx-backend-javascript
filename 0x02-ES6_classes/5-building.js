69fb594d76f065b9b9e7cb0118ad578825ea2f04
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-26 10:14:18 +0300

    5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,12 @@
-#!/usr/bin/node
-/**
-** module for building
-*/
-
 export default class Building {
-   constructor(sqft) {
-    if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-       throw new Error('Class extending Building must override evacuationWarningMessage');
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-     if (typeof sqft !== 'number') {
-        throw new TypeError('sqft must be a number');
-     }
-     this._sqft = sqft;
-   }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-       return this._sqft;
-    }
-    set sqft(value) {
-       return this._sqft = value;
-    }
-
-    
+  get sqft() {
+    return this._sqft;
+  }
 }
