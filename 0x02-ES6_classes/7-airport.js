024d35086cc3b36c2c1192fa4e09ad8e1a722a2b
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-26 10:18:15 +0300

    7. Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,21 +1,26 @@
-#!/usr/bin/node
-/**
-** module for Airport
-**/
-
 export default class Airport {
-   constructor(name, code) {
-      if (typeof name !== 'string') {
-         throw new TypeError ('name must be a string');
-      }
-      if (typeof code !== 'string') {
-         throw new TypeError ('code must be a string');
-      }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-      this._name = name;
-      this._code = code;
-   }
-   toString() {
-     return `[object ${this._code}]`;
-   }
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
 }
