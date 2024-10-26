a82e618be083a3bcc8c884d928fc5fbe223ae72f
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-26 10:28:53 +0300

    0. Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,7 @@
-#!/usr/bin/node
-
 export default function getListStudents() {
-   const info = [
-     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-     { id: 2, firstName: 'James', location: 'Columbia' },
-     { id: 5, firstName: 'Serena', location: 'San Francisco' }
-   ];
-   return info;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
