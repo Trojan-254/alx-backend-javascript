11db01f0fdf916b28cb92e087b2c873921c735dc
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-26 10:19:55 +0300

    8. Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,22 +1,14 @@
-#!/usr/bin/node
-
 export default class HolbertonClass {
-    constructor(size, location) {
-       if (typeof size !== 'number') {
-          throw new TypeError('size must be a number');
-       }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-       if (typeof location !== 'string') { 
-          throw new TypeError('location must be a string');
-       }
-       this._size = size;
-       this._location = location;
-    }
+  valueOf() {
+    return this._size;
+  }
 
-    valueOf() {
-       return this._size;
-    }
-    toString() {
-      return this._location
-    }
+  toString() {
+    return this._location;
+  }
 }
