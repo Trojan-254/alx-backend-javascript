cccefa45588f2e547e14a1799ba519dbe2dbdae1
Samwuel Simiyu <samwuels003@gmail.com>
2024-10-26 10:16:14 +0300

    6. Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,29 +1,20 @@
-#!/usr/bin/node
-/**
-** module for building inheritance
-*/
-
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-      super(sqft);
-      this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get floors() { 
-      return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    set floors(value) {
-        if (typeof floors !== 'number') {
-         throw new TypeError('floors must be a number');
-        } 
-        return this._floors = value;
-    }
+  set floors(value) {
+    this._floors = value;
+  }
 
-    evacuationWarningMessage() {
-
-       return `Evacuate slowly the ${this._floors} floors`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
